2a3f86e0e2128b48a752a7ae7b704c30e722c71e
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-20 15:30:33 +0100

    correction

--- Program.cs
+++ Program.cs
@@ -11,12 +11,15 @@ namespace Boucle08
         static void Main(string[] args)
         {
             {
-                int[] pairs = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-
+                int[] pairs = new int[] { 1, 2, 443, 4, 5, 6, 77, 8, 90, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
                 foreach (int pair in pairs)
                 {
+                    // utilisation du modulo pour sélectionner les nombres pouvant se diviser par 2
+                    // le modulo "%" indique le reste d'une division
                     if (pair % 2 == 0)
-                    Console.WriteLine(pair);
+                    {
+                        Console.WriteLine(pair);
+                    }
                 }
             }
         }
